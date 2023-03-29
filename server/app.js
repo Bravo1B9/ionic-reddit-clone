7e40f109337142b2537c40478bfb940c476f825f
Bravo1B9 <brofe93@gmail.com>
2023-03-29 16:43:11 +0000

    Establish database connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const PORT = 3000;
 
 const app = express();
+
+const dbUri = 'mongodb+srv://brendon:pass123@cluster0.ysgq09a.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log('Connected to database');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectToDatabase();
 
 app.listen(PORT, () => {
   console.log(`App listening on port `)
